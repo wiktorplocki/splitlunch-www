d9e96d3276b6bec1872f383a8e195bf2162e39a7
wiktor Płocki <wiktor.plocki@gmail.com>
2019-04-21 19:28:01 +0200

    Add useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (callback, validate) => {
+  const [values, setValue] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors]);
+
+  const handleSubmit = event => {
+    if (event) {
+      event.preventDefault();
+      setIsSubmitting(true);
+      setErrors(validate(values));
+    }
+  };
+
+  const handleChange = event => {
+    event.persist();
+    setValue(values => ({
+      ...values,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errors
+  };
+};
+
+export default useForm;
