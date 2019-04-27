ceef08c5873bf754909f2201fcebd443806853f9
wiktor Płocki <wiktor.plocki@gmail.com>
2019-04-27 10:51:32 +0200

    Handle secrets via now-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+require('now-env');
 const path = require('path');
 const glob = require('glob');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -19,114 +19,109 @@ const PATHS = {
   src: path.join(__dirname, 'src')
 };
 
-module.exports = env => {
-  return {
-    entry: './src/App.js',
-    output: {
-      filename: 'main.[hash].js',
-      chunkFilename: '[name].[hash].js'
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': env ? env : process.env.NODE_ENV
-      }),
-      new MiniCssExtractPlugin({
-        filename: 'styles.[hash].css',
-        chunkFilename: '[name].[hash].css'
-      }),
-      new PurgecssWebpackPlugin({
-        whitelist: ['html', 'body'],
-        paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-        content: ['./src/**/*.html', './src/**/*.js'],
-        extractors: [
-          {
-            extractor: TailwindExtractor,
-            extensions: ['html', 'js']
-          }
-        ]
-      }),
-      new HtmlWebpackPlugin({
-        title: 'SplitLunch',
-        inject: false,
-        template: require('html-webpack-template'),
-        appMountId: 'root',
-        baseHref: '/'
-      }),
-      new HtmlWebpackExternalsPlugin({
-        externals: [
-          {
-            module: 'google-nunito',
-            entry: {
-              path:
-                'https://fonts.googleapis.com/css?family=Nunito:300,400,600,700&amp;subset=latin-ext',
-              type: 'css'
-            }
-          }
-        ]
-      })
-    ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          default: false,
-          vendors: {
-            name: 'vendors',
-            chunks: 'all',
-            test: /node_modules/,
-            priority: 20
-          }
-        }
-      },
-      concatenateModules: true
-    },
-    devServer: {
-      contentBase: path.resolve(__dirname, 'dist'),
-      compress: true,
-      historyApiFallback: true
-    },
-    module: {
-      rules: [
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    filename: 'main.[hash].js',
+    chunkFilename: '[name].[hash].js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[hash].css',
+      chunkFilename: '[name].[hash].css'
+    }),
+    new PurgecssWebpackPlugin({
+      whitelist: ['html', 'body'],
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+      content: ['./src/**/*.html', './src/**/*.js'],
+      extractors: [
         {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            plugins: ['@babel/plugin-syntax-dynamic-import'],
-            presets: [
-              [
-                '@babel/preset-env',
-                { targets: '> 0.25%, not dead, not ie 11, not op_mini all' }
-              ],
-              '@babel/preset-react'
-            ]
-          }
-        },
-        {
-          test: /\.css$/,
-          exclude: /node_modules/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-                importLoaders: 1
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: () => [
-                  PostCssImport(),
-                  PostcssPresetEnv(),
-                  Tailwind('./tailwind.js')
-                ]
-              }
-            }
-          ]
+          extractor: TailwindExtractor,
+          extensions: ['html', 'js']
         }
       ]
-    }
-  };
+    }),
+    new HtmlWebpackPlugin({
+      title: 'SplitLunch',
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'root',
+      baseHref: '/'
+    }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: 'google-nunito',
+          entry: {
+            path:
+              'https://fonts.googleapis.com/css?family=Nunito:300,400,600,700&amp;subset=latin-ext',
+            type: 'css'
+          }
+        }
+      ]
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          test: /node_modules/,
+          priority: 20
+        }
+      }
+    },
+    concatenateModules: true
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['@babel/plugin-syntax-dynamic-import'],
+          presets: [
+            [
+              '@babel/preset-env',
+              { targets: '> 0.25%, not dead, not ie 11, not op_mini all' }
+            ],
+            '@babel/preset-react'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                PostCssImport(),
+                PostcssPresetEnv(),
+                Tailwind('./tailwind.js')
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
