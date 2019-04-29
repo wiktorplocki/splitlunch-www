6201eb93640245dcb728ca51a8d70e813657881c
wiktor Płocki <wiktor.plocki@gmail.com>
2019-04-29 19:50:51 +0200

    GraphQL schemas and config

--- src/queries/LoginQuery.js
+++
@@ -1,9 +0,0 @@
-const LoginQuery = `query Login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    userId
-    token
-    tokenExpiry
-  }
-}`;
-
-export default LoginQuery;
