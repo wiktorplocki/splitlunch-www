9b9baa136984e7dbff463d83f07391ccfc227d86
wiktor Płocki <wiktor.plocki@gmail.com>
2019-05-14 10:34:59 +0200

    Remove useless Fragment

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -2,16 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ListItem = ({ data: { name, rest } }) => (
-  <React.Fragment>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="px-6 py-4 flex">
-        <div className="column">
-          <div className="font-bold text-xl mb-2">{name}</div>
-          <p className="text-grey-darker text-base">{rest}</p>
-        </div>
+  <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="px-6 py-4 flex">
+      <div className="column">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-grey-darker text-base">{rest}</p>
       </div>
     </div>
-  </React.Fragment>
+  </div>
 );
 
 ListItem.propTypes = {
