5fc29827f2c8684567f62c3752089115aed87fc4
wiktor Płocki <wiktor.plocki@gmail.com>
2019-12-27 17:45:09 +0100

    Update FlexLayout

--- src/components/Layout/FlexLayout.js
+++ src/components/Layout/FlexLayout.js
@@ -2,27 +2,54 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+function flexDirection(direction) {
+  switch (direction) {
+    case "row":
+      return "row";
+    case "column":
+      return "column";
+    case "row-reverse":
+      return "row-reverse";
+    case "column-reverse":
+      return "column-reverse";
+    default:
+      return "row";
+  }
+}
+
 const StyledFlexLayout = styled.div`
   display: flex;
   height: 100vh;
   flex: 1;
+  flex-direction: ${props => props.direction && flexDirection(props.direction)};
   align-items: ${props => props.absoluteCenter && "center"};
   justify-content: ${props => props.absoluteCenter && "center"};
 `;
 
-const FlexLayout = ({ absoluteCenter, children, className }) => (
-  <StyledFlexLayout absoluteCenter={absoluteCenter} className={className}>
+const FlexLayout = ({ absoluteCenter, direction, children, className }) => (
+  <StyledFlexLayout
+    absoluteCenter={absoluteCenter}
+    direction={direction}
+    className={className}
+  >
     {children}
   </StyledFlexLayout>
 );
 
 FlexLayout.defaultProps = {
   absoluteCenter: false,
+  direction: "row",
   className: undefined
 };
 
 FlexLayout.propTypes = {
   absoluteCenter: PropTypes.bool,
+  direction: PropTypes.oneOf([
+    "row",
+    "column",
+    "row-reverse",
+    "column-reverse"
+  ]),
   className: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]).isRequired
 };
